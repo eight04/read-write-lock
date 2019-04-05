e05cbd677f5f20a3c1d816775582a8ef88baccdf
eight <eight04@gmail.com>
2019-04-05 10:51:57 +0800

    Test: more lock pool test

--- test/lock-pool.js
+++ test/lock-pool.js
@@ -34,4 +34,51 @@ describe("lock pool", () => {
     await p;
     assert.deepStrictEqual(q, [1, 3, 2]);
   });
+  
+  it("read should run parallel", async () => {
+    const locks = createLockPool();
+    const q = [];
+    const p = locks.read(["foo", "bar"], async () => {
+      q.push(1);
+      await delay();
+      q.push(2);
+    });
+    locks.read(["foo"], () => {
+      q.push(3);
+    });
+    await p;
+    assert.deepStrictEqual(q, [1, 3, 2]);
+  });
+  
+  it("manually release", async () => {
+    const locks = createLockPool();
+    const q = [];
+    locks.read(["foo", "bar"], release => {
+      q.push(1);
+      setTimeout(() => {
+        q.push(2);
+        release();
+      }, 10);
+    });
+    const p = locks.write(["foo"], () => {
+      q.push(3);
+    });
+    await p;
+    assert.deepStrictEqual(q, [1, 2, 3]);
+  });
+  
+  it("release automatically if the callback throws", async () => {
+    const locks = createLockPool();
+    const q = [];
+    const p1 = locks.read(["foo", "bar"], release => { // eslint-disable-line no-unused-vars
+      q.push(1);
+      throw new Error("foo");
+    });
+    const p2 = locks.write(["foo"], () => {
+      q.push(2);
+    });
+    await p2;
+    assert.deepStrictEqual(q, [1, 2]);
+    await assert.rejects(p1);
+  });
 });
